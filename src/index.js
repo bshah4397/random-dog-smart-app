e1f36b7af508474cb916797b9aed957a6866de54
Bhavya Shah <bshah4397@gmail.com>
2022-03-31 11:16:30 -0400

    update: edited clientId to match cerners Random 2 app

--- src/index.js
+++ src/index.js
@@ -11,7 +11,8 @@ const smartLaunch = () => {
 
   FHIR.oauth2
     .init({
-      clientId: '20560ea5-f224-4658-b667-4e6bab935c85',
+      // clientId: '20560ea5-f224-4658-b667-4e6bab935c85',
+      clientId: '29f8073d-e615-406f-9ab8-d22922829a6c',
       scope: 'launch/patient openid profile'
     })
     .then(client => {
