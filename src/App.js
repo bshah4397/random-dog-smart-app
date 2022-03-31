48b02bb18a45f87714cd469cea98de766b561006
Bhavya Shah <bhavyashah@Bhavyas-MacBook-Pro.local>
2022-03-30 20:03:31 -0400

    add: initialCommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { PatientVisualizer } from 'fhir-visualizers';
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image'
+import { Container, Row, Col, } from 'react-bootstrap';
+import { dogPuns } from './dogPuns';
 
-function App() {
+
+
+function App(props) {
+  const client = props.client;
+  const [patient, setPatient] = useState(null);
+  const [dogImage, setDogImage] = useState(null);
+  const [dogPunSelected, setDogPunSelected] = useState(null);
+
+  useEffect(() => { client.patient.read().then((patient) => setPatient(patient)); });
+
+  useEffect(() => {
+    const fetchDogImage = async () => {
+      const response = await fetch('https://random.dog/woof.json?filter=mp4,webm');
+      const data = await response.json();
+      setDogImage(data.url);
+      setDogPunSelected(dogPuns[Math.floor(Math.random() * dogPuns.length)]);
+    }
+
+    fetchDogImage();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', }}>
+      <Container>
+
+        <Row className="justify-content-md-center">
+          <Col md="auto"><h3>{dogPunSelected}</h3></Col>
+        </Row>
+
+        <Row className="justify-content-md-center">
+          {dogImage ? <Col md="auto"><Image src={dogImage} height="400px" /></Col> : <h2>Loading...</h2>}
+        </Row>
+
+        <Row className="justify-content-md-center">
+          {patient ? <Col md="auto"><PatientVisualizer patient={patient} /></Col> : <h1>Loading</h1>}
+        </Row>
+
+      </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import FHIR from 'fhirclient';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const smartLaunch = () => {
+
+  FHIR.oauth2
+    .init({
+      clientId: '20560ea5-f224-4658-b667-4e6bab935c85',
+      scope: 'launch/patient openid profile'
+    })
+    .then(client => {
+      ReactDOM.render(<App client={client} />, rootElement);
+    });
+};
+
+smartLaunch();
