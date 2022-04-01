2cb022641f34815896aaaabf23ea7ca3fe847dfa
Bhavya Shah <bshah4397@gmail.com>
2022-04-01 15:57:51 +0000

    add: console log to check client

--- src/index.js
+++ src/index.js
@@ -11,12 +11,13 @@ const smartLaunch = () => {
 
   FHIR.oauth2
     .init({
-      // clientId: '20560ea5-f224-4658-b667-4e6bab935c85',
-      // clientId: '29f8073d-e615-406f-9ab8-d22922829a6c',
-      clientId: '2facafab-e9a7-44aa-9f3d-5535984cd7ff',
+      // clientId: '20560ea5-f224-4658-b667-4e6bab935c85',  // Random clientId
+      // clientId: '29f8073d-e615-406f-9ab8-d22922829a6c',  // Cerner clientId
+      clientId: '2facafab-e9a7-44aa-9f3d-5535984cd7ff',     //  
       scope: 'launch/patient openid profile'
     })
     .then(client => {
+      console.log("SmartApp:Client => ", client);
       ReactDOM.render(<App client={client} />, rootElement);
     });
 };
