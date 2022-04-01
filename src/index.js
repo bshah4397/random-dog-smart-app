b69ace7ba1f1c72d23426e0c7900e09b79f08c91
Bhavya Shah <bshah4397@gmail.com>
2022-04-01 15:17:01 +0000

    update: clientId

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ const smartLaunch = () => {
   FHIR.oauth2
     .init({
       // clientId: '20560ea5-f224-4658-b667-4e6bab935c85',
-      clientId: '29f8073d-e615-406f-9ab8-d22922829a6c',
+      // clientId: '29f8073d-e615-406f-9ab8-d22922829a6c',
+      clientId: '2facafab-e9a7-44aa-9f3d-5535984cd7ff',
       scope: 'launch/patient openid profile'
     })
     .then(client => {
