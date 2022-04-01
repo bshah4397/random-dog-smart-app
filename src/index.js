cff53519e0123a6f1cf61e600e8fd3283bea2b3e
Bhavya Shah <bshah4397@gmail.com>
2022-04-01 19:31:37 +0000

    update: added launch/patient scope

--- src/index.js
+++ src/index.js
@@ -14,8 +14,8 @@ const smartLaunch = () => {
       // clientId: '20560ea5-f224-4658-b667-4e6bab935c85',  // Random clientId
       // clientId: '29f8073d-e615-406f-9ab8-d22922829a6c',  // Cerner clientId
       clientId: '0oamrpbycykHySdrd1t7',                     // XDGE clientId
-      // scope: 'launch/patient openid profile'
-      scope: 'launch openid profile'
+      scope: 'launch/patient openid profile'
+      // scope: 'launch openid profile'
     })
     .then(client => {
       console.log("SmartApp:Client => ", client);
