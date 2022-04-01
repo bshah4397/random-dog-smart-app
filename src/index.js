ca096be5c82efdefb8f5ac28debdeb577c37b788
Bhavya Shah <bshah4397@gmail.com>
2022-04-01 16:03:05 +0000

    update: clientId

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const smartLaunch = () => {
     .init({
       // clientId: '20560ea5-f224-4658-b667-4e6bab935c85',  // Random clientId
       // clientId: '29f8073d-e615-406f-9ab8-d22922829a6c',  // Cerner clientId
-      clientId: '2facafab-e9a7-44aa-9f3d-5535984cd7ff',     //  
+      clientId: '0oamrpbycykHySdrd1t7',                     // XDGE clientId
       scope: 'launch/patient openid profile'
     })
     .then(client => {
