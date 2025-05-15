4884b47ef036139a6918b9a4f5c701fe73a76a01
Bhavya Shah <bshah@athenahealth.com>
2025-05-15 14:34:13 -0400

    Changed static app

--- src/App.js
+++ src/App.js
@@ -21,13 +21,13 @@ function App({ patient, meds }) {
       
           // Notify parent window (e.g., iframe parent or opener parent, if opened from a "new window" button)
           if (window.parent && window.parent !== window) { // Check if it's truly embedded
-            console.log(`(window.parent.postMessage) Sending the following message to (${targetOrigin}) :=> `, messagePayload);
+            console.log(`(From the App) (window.parent.postMessage) Sending the following message to (${targetOrigin}) :=> `, messagePayload);
             window.parent.postMessage(messagePayload, targetOrigin);
           }
       
           if (window.opener) {
             try {
-              console.log(`(window.opener.postMessage) Sending the following message to (${targetOrigin}) :=> `, messagePayload);
+              console.log(`(From the App) (window.opener.postMessage) Sending the following message to (${targetOrigin}) :=> `, messagePayload);
               window.opener.postMessage(messagePayload, targetOrigin);
             } catch (e) {
               // Log error if postMessage to opener fails (e.g., if the opener window was closed)
@@ -39,9 +39,8 @@ function App({ patient, meds }) {
 
       <button
           onClick = {() => {
-            const parentOrigin = window.parent.location.origin;
-            console.log(`(window.opener.postMessage) Sending the following message to (${parentOrigin}) :=> `, { type: 'embeddedAppAPIMessage', method: 'notifyPatientDataChange', methodVersion: "1.0.0" });
-            window.parent.postMessage({ type: 'embeddedAppAPIMessage', method: 'notifyPatientDataChange', methodVersion: "1.0.0" }, parentOrigin);
+            console.log(`(From the App) (window.parent.postMessage) Sending the following message to (*}) :=> `, { type: 'embeddedAppAPIMessage', method: 'notifyPatientDataChange', methodVersion: "1.0.0" });
+            window.parent.postMessage({ type: 'embeddedAppAPIMessage', method: 'notifyPatientDataChange', methodVersion: "1.0.0" }, '*');
           }}
       > Refresh Patient - Our code </button>
   </div>
